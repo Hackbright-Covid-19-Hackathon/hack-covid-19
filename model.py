339b5dc79a39a7088c81a88c201a772cc15ad749
Lauren Maghran <maghranlauren@gmail.com>
2020-03-28 21:16:27 +0000

    Database and tests working

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+#db createdb volunteerdb
+# python3 -i model.py
+ ## in python3 interactive db.createall()
 
 
 ##############################################################################
@@ -21,15 +24,14 @@ class User(db.Model):
     is_asker= db.Column(db.Boolean(), nullable= True)
     is_vol= db.Column(db.Boolean(), nullable= True)
     trust_score= db.Column(db.String(15), nullable=True)
-    trip = db.relationship("Trip",
-                           backref=db.backref("relations", 
-                           order_by=user_id))
-
+    # trip = db.relationship("Trip",
+    #                        backref=db.backref("relations", 
+    #                        order_by=user_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<User user_id={self.user_id} zipcode={self.zipcode}>"
+        return f"<User user_id={self.user_id} zipcode={self.uzipcode}>"
 
 class Relational(db.Model):
 
@@ -37,17 +39,17 @@ class Relational(db.Model):
 
     relation_id= db.Column(db.Integer, autoincrement=True, primary_key=True)
     r_asker_id= db.Column(db.Integer, db.ForeignKey('users.user_id'), index=True)
-    r_vol_id= db.Column(db.Integer, db.ForeignKey('users.user_id'), index=True)
-    r_trip_id = db.Column(db.Integer, db.ForeignKey('trips.trip_id'), index=True)
+    r_vol_id= db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable= True)
+    r_trip_id = db.Column(db.Integer, db.ForeignKey('trips.trip_id'), nullable= True)
 
-    #relationships with the user table
-    r_asker_id_rel = db.relationship('User', 
-                                    foreign_keys="[User.user_id]", 
-                                    backref='relational_asker')
+#     # #relationships with the user table
+#     # r_asker_id_rel = db.relationship('User', 
+#     #                                 foreign_keys="[User.user_id]", 
+#     #                                 backref='relational_asker')
 
-    r_vol_id_rel = db.relationship('User', 
-                                    foreign_keys="[User.user_id]", 
-                                    backref='relational_volunteer')
+#     # r_vol_id_rel = db.relationship('User', 
+#     #                                 foreign_keys="[User.user_id]", 
+#     #                                 backref='relational_volunteer')
 
 class Trip(db.Model):
     """Items that a user requests"""
@@ -79,7 +81,52 @@ class Wishlist(db.Model):
 
         return f"""<Item ={self.item_description}>"""
 
-##############################################################################
+# # db.create_all()
+
+
+# c1 = User(
+# user_full_name= "Faker Fakey",
+# email = "fake@gmail.com",
+# password = "123",
+# uzipcode = "94805",
+# is_asker= True,
+# is_vol= False
+# )
+
+    # c2 = User(
+    # user_full_name= "Faker RE Fakey",
+    # email = "@gmail.com",
+    # password = "1235",
+    # uzipcode = "94121",
+    # is_asker= False,
+    # is_vol= True
+    # )
+
+    # r1 = Relational(
+    # r_asker_id= 1,
+    # r_vol_id_rel= 1,
+    # r_asker_id_rel= 1
+    # # r_vol_id= 2, 
+    # # r_trip_id = 1
+    # )
+
+    # t1 = Trip(
+    # trip_zipcode= "94805",
+    # user_id= 1,
+    # wishlist = "I want a kitten",
+    # item_progress= "En Route"
+    # ) 
+
+    # w1= Wishlist(
+    #     )
+
+
+## To add data to the db:
+
+# example_data()
+# db.session.add_all([c1, c2, r1, t1, w1])
+# db.session.commit()
+
 # Helper functions
 
 def connect_to_db(app, db_uri='postgresql:///volunteerdb'):
@@ -90,8 +137,6 @@ def connect_to_db(app, db_uri='postgresql:///volunteerdb'):
     db.app = app
     db.init_app(app)
 
-
-# db.create_all()
 
 if __name__ == "__main__":
     from server import app
--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from model import connect_to_db, db, User, Relational, Trip, Wishlist
+from model import connect_to_db, db, User
+# , Relational, Trip, Wishlist
 
 
 app = Flask(__name__)
