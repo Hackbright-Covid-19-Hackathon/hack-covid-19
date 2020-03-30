ca9c3070266bebe34c8e8a60f9a11f7c904fe3f2
vagrant <vagrant@vagrant.vm>
2020-03-30 04:11:46 +0000

    fixed about page

--- server.py
+++ server.py
@@ -336,7 +336,7 @@ def about():
 
 if __name__ == "__main__": 
 
-    app.debug = True #pragma: no cover
+    app.debug = False #pragma: no cover
     connect_to_db(app) #pragma: no cover
     DebugToolbarExtension(app) #pragma: no cover
     app.run(host="0.0.0.0") #pragma: no cover
